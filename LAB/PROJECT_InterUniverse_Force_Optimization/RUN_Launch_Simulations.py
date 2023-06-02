f1c2142b615fa0962f876d6c7736516865c3dd26
Xabier_Oyanguren_Asua <oiangu9@gmail.com>
2023-06-02 17:09:37 +0200

    Full pipeline rady for simulations!

--- LAB/PROJECT_InterUniverse_Force_Optimization/RUN_Launch_Simulations.py
+++ LAB/PROJECT_InterUniverse_Force_Optimization/RUN_Launch_Simulations.py
@@ -3,6 +3,7 @@ import multiprocessing as mp
 import time
 import sys
 
+
 if __name__ == "__main__":
     # SIMULATION PARAMETERS ##################################
     args = sys.argv # The path to the settings file should be given
@@ -24,8 +25,28 @@ if __name__ == "__main__":
             )
         return arguments[0] #ID
     #run_coordinator_and_return(arguments_for_coordinators[1])
-    # We initiate a pool of num_workers processes
-    pool = mp.Pool(num_coordinators)
+    # We initiate a pool of num_workers processes-> Pythoin does not allow pools to generate children pools!!! So we have to do this with processes
+    wait= 180 # number of seconds to wait till re-check if ready
+    t=0
+    processes = []
+    for arguments_for_coordinatork in arguments_for_coordinators:
+        while len(processes)>=num_coordinators:
+            did=[]
+            for k,process in enumerate(processes):
+                if not process.is_alive():
+                    process.join()
+                    did.append(k)
+                    print(f">>>> t={t}s Done one!")
+            for d in did:
+                processes.pop(d)
+            t+=wait
+            time.sleep(wait)
+        processes.append( mp.Process(target=run_coordinator_and_return,
+                args=(arguments_for_coordinatork,)) )
+        processes[-1].start()
+
+    '''
+    pool = Pool(num_coordinators)
     result_tickets = []
     IDs=[]
     for arguments_for_coordinatork in arguments_for_coordinators:
@@ -51,6 +72,7 @@ if __name__ == "__main__":
         result_ticket.wait()
     pool.close()
     pool.join()
+    '''
     # we remove the state file now that it has finished and place a done flag
     with open(f".//FINISHED.txt", 'w') as f:
         f.write(f"{len(arguments_for_coordinators)} coordinators called and done in t={t} s={t/60:.3} min={t/3600:.3} h\n")
